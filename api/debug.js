8818cd1a41b95d8060bb0fd0be5a38b2f5f54bdf
sseomkochi-ai <sseomkochi@gmail.com>
2026-07-16 01:29:36 +0900

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({
-    TOSS_SECRET_KEY: !!process.env.TOSS_SECRET_KEY,
-    SUPABASE_URL: !!process.env.SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE: !!process.env.SUPABASE_SERVICE_ROLE,
-    ANTHROPIC_API_KEY: !!process.env.ANTHROPIC_API_KEY
-  });
-};
